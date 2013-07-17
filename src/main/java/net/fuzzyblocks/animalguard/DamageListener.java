f7b09f3122ff1187559dcd5967f363e04c6597ea
LankyLord <scottkendall1@gmail.com>
2013-07-17 19:05:35 +0100

    Modified DamageListener.

--- src/main/java/net/fuzzyblocks/animalguard/DamageListener.java
+++ src/main/java/net/fuzzyblocks/animalguard/DamageListener.java
@@ -1,7 +1,6 @@
 package net.fuzzyblocks.animalguard;
 
 // import com.sk89q.worldguard.protection.managers.RegionManager;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
@@ -17,96 +16,73 @@ import java.util.List;
 
 public class DamageListener implements Listener {
 
-    String fail = ChatColor.DARK_RED + "You cannot attack mobs here!";
+    String cannotKillMobs = ChatColor.DARK_RED + "You cannot attack mobs here!";
     public static AnimalGuard plugin;
-    long lnt;
+    public boolean debug = plugin.getConfig().getBoolean("debug");
+    private List<String> protectFromPlayer = plugin.getConfig().getStringList("protect-from-player");
+    private List<String> protectFromMonster = plugin.getConfig().getStringList("protect-from-monsters");
 
     public DamageListener(AnimalGuard instance) {
         plugin = instance;
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onAttacked(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player) {
-            String entity = event.getEntityType().toString();
-            List<String> protect = plugin.getConfig().getStringList("protect-from-player");
-            Boolean debug = plugin.getConfig().getBoolean("debug");
-            Player player = (Player) event.getDamager();
-            Location loc = event.getEntity().getLocation();
-            if (debug == true)
+    public void onAttacked(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Player) {
+            String entity = e.getEntityType().toString();
+            Player player = (Player) e.getDamager();
+            Location loc = e.getEntity().getLocation();
+            if (debug)
                 player.sendMessage(entity);
-            // RegionManager rm = plugin.getWorldGuardPlugin().getRegionManager(loc.getWorld());
-            if (protect.contains(entity))
+            if (protectFromPlayer.contains(entity))
                 if (plugin.getWorldGuardPlugin().canBuild(player, loc) || player.hasPermission("animalprotect.bypass")) {
-                    event.setCancelled(false);
-                    if (debug == true) {
-                        player.sendMessage(event.getDamager().getType().toString() + " Attacked " + event.getEntity().getType().toString());
-                        player.sendMessage("Attack successful");
-                    }
+                    e.setCancelled(false);
+                    if (debug)
+                        sendDebugMessage(player, false, e.getDamager().getType().toString(), e.getEntity().getType().toString());
                 } else {
-                    event.setCancelled(true);
-                    if (debug) {
-                        player.sendMessage(event.getDamager().getType().toString() + " Attacked " + event.getEntity().getType().toString());
-                        player.sendMessage("Attack failed");
-                    }
-                    player.sendMessage(fail);
-                    if (plugin.getConfig().getBoolean("notify"))
-                        notifyAdmin(player);
+                    e.setCancelled(true);
+                    player.sendMessage(cannotKillMobs);
+                    if (debug)
+                        sendDebugMessage(player, true, e.getDamager().getType().toString(), e.getEntity().getType().toString());
                 }
-
         }
+
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onAttackArrow(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Arrow) {
-            Projectile arrow = (Arrow) event.getDamager();
-            String entity = event.getEntity().getType().toString();
-            Boolean debug = plugin.getConfig().getBoolean("debug");
-            List<String> pfa = plugin.getConfig().getStringList("protect-from-player");
-            if (arrow.getShooter() instanceof Player && pfa.contains(entity)) {
+    public void onAttackArrow(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Arrow) {
+            Projectile arrow = (Arrow) e.getDamager();
+            String entity = e.getEntity().getType().toString();
+            if (arrow.getShooter() instanceof Player && protectFromPlayer.contains(entity)) {
                 Player player = (Player) arrow.getShooter();
-                Location loc = event.getEntity().getLocation();
+                Location loc = e.getEntity().getLocation();
                 if (plugin.getWorldGuardPlugin().canBuild(player, loc) || player.hasPermission("animalprotect.bypass")) {
-                    event.setCancelled(false);
-                    if (debug) {
-                        player.sendMessage(event.getDamager().getType().toString() + " Attacked " + event.getEntity().getType().toString());
-                        player.sendMessage("Attack successful");
-                    }
+                    e.setCancelled(false);
+                    if (debug)
+                        sendDebugMessage(player, false, e.getDamager().getType().toString(), e.getEntity().getType().toString());
                 } else {
-                    event.setCancelled(true);
-                    if (debug) {
-                        player.sendMessage(event.getDamager().getType().toString() + " Attacked " + event.getEntity().getType().toString());
-                        player.sendMessage("Attack failed");
-                    }
-                    player.sendMessage(fail);
-                    if (plugin.getConfig().getBoolean("notify"))
-                        notifyAdmin(player);
+                    e.setCancelled(true);
+                    player.sendMessage(cannotKillMobs);
+                    if (debug)
+                        sendDebugMessage(player, true, e.getDamager().getType().toString(), e.getEntity().getType().toString());
                 }
             }
         }
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onMonsterDamage(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Monster) {
-            List<String> pfm = plugin.getConfig().getStringList("protect-from-monsters");
-            if (pfm.contains(event.getEntityType().toString()))
-                event.setCancelled(true);
-        }
+    public void onMonsterDamage(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Monster)
+            if (protectFromMonster.contains(e.getEntityType().toString()))
+                e.setCancelled(true);
     }
 
-    public void notifyAdmin(Player player) {
-        long timesincelastnote = System.currentTimeMillis() - lnt;
-        if (timesincelastnote > plugin.getConfig().getInt("notify-interval") * 1000) {
-            lnt = System.currentTimeMillis();
-            if (DamageListener.plugin.getConfig().getBoolean("notify"))
-                for (Player onlinePlayer : Bukkit.getServer().getOnlinePlayers())
-                    // Get a list of online players and check if they have permission/op //
-                    if (onlinePlayer.hasPermission("animalprotect-notify") || onlinePlayer.isOp()) {
-                        onlinePlayer.sendMessage(plugin.fail + player.getName() + " " + "Attempted to kill protected animals");
-                        plugin.getLogger().info(player.getName() + " " + "Attempted to kill protected animals");
-                    }
-        }
+    private void sendDebugMessage(Player p, Boolean fail, String damager, String victim) {
+        p.sendMessage(damager + " attacked " + victim);
+        if (fail)
+            p.sendMessage("Attack failed");
+        else
+            p.sendMessage("Attack successful");
     }
 }
